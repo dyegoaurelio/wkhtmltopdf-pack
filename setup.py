21acff7d6408dfbda2d844b270fd8d05c1758557
Dyego <dyegoaurelio@gmail.com>
2023-10-26 18:06:18 -0300

    add libX11

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
                 "lib/libjpeg.so.62",
                 "lib/libXrender.so.1",
                 "lib/libXext.so.6",
+                "lib/libX11.so.6",
                 # "lib/libwkhtmltox.so.0",
                 # "lib/libwkhtmltox.so.0.12",
                 # "lib/libwkhtmltox.so.0.12.6",
